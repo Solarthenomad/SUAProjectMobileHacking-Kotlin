6773ab63c159593ab3d5298c01f8f0f7a2e1f574
solarthenomad <oakikim@ewhain.net>
2023-02-05 17:31:08 +0900

    실험적 커밋

--- app/src/main/java/com/example/sogaeting/auth/JoinActivity.kt
+++ app/src/main/java/com/example/sogaeting/auth/JoinActivity.kt
@@ -1,11 +1,31 @@
 package com.example.sogaeting.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.sogaeting.R
+import com.google.android.material.textfield.TextInputEditText
 
 class JoinActivity : AppCompatActivity() {
+
+    private val TAG = "JoinActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        val joinBtn : Button = findViewById<Button>(R.id.joinBtn)
+        joinBtn.setOnClickListener {
+
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+
+            val email = findViewById<TextInputEditText>(R.id.emailArea)
+            val pwd = findViewById<TextInputEditText>(R.id.pwdArea)
+
+
+            Log.d(TAG, email.text.toString())
+            Log.d(TAG,pwd.text.toString())
+        }
     }
 }
